1aeb293cb80aa293446050e662683971f2d8cf41
Basit ali <basitalyshah51214@gmail.com>
2025-03-25 03:00:31 -0700

    Add settings for VSCode and initialize dataList with user profiles

---
+++ lib/constants.dart
@@ -0,0 +1,32 @@
+List<Map<String, dynamic>> dataList = [
+  {
+    "username": "Ali",
+    "email": "Ali@gmail.com",
+    "profile":
+        "https://media.istockphoto.com/id/1500308602/photo/happy-black-man-mature-or-portrait-in-finance-office-about-us-company-profile-picture-or-ceo.jpg?s=612x612&w=0&k=20&c=3BWt_eT7QaaiGx4zI_K63pnntIp5Cv1qW8Pw-_bSlm8="
+  },
+  {
+    "username": "Hassan",
+    "email": "Hassan@gmail.com",
+    "profile":
+        "https://t4.ftcdn.net/jpg/06/08/55/73/360_F_608557356_ELcD2pwQO9pduTRL30umabzgJoQn5fnd.jpg"
+  },
+  {
+    "username": "Fayyaz",
+    "email": "Fayyaz@gmail.com",
+    "profile":
+        "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+  },
+  {
+    "username": "Jawad",
+    "email": "Jawad@gmail.com",
+    "profile":
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw0PDKrErulLlbJkbv5KtsCeICczdgJSyurA&s"
+  },
+  {
+    "username": "Irfan",
+    "email": "Irfan@gmail.com",
+    "profile":
+        "https://media.istockphoto.com/id/1476170969/photo/portrait-of-young-man-ready-for-job-business-concept.jpg?s=612x612&w=0&k=20&c=w8SlKv-4u6xYyU07CXeBRvfW6F0iYx-a7HR2ChM8ZbU="
+  },
+];
