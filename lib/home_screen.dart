ff2d7daf734eee47802d534e57634a0043759926
Basit ali <basitalyshah51214@gmail.com>
2025-03-26 03:51:34 -0700

    make components

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firstapp/constants.dart';
+import 'package:firstapp/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,25 +8,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Expanded(
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: dataList.length,
-            itemBuilder: (context, index) {
-              final users = dataList[index];
-              return Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: NetworkImage(users['profile']),
-                      fit: BoxFit.contain),
-                ),
-              );
-            }),
-      ),
-    );
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            CustomButton(
+              title: "Button One",
+              backgroundColor: Colors.black,
+              ontap: () {
+                debugPrint("button one pressed");
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomButton(
+              title: "Button Two",
+              backgroundColor: Colors.green,
+              ontap: () {
+                debugPrint("button two pressed");
+              },
+            ),
+          ],
+        )
+        // Expanded(
+        //   child: ListView.builder(
+        //       itemCount: dataList.length,
+        //       itemBuilder: (context, index) {
+        //         final users = dataList[index];
+        //         return Container(
+        //           height: 100,
+        //           width: 100,
+        //           decoration: BoxDecoration(
+        //             shape: BoxShape.circle,
+        //             image: DecorationImage(
+        //                 image: NetworkImage(users['profile']),
+        //                 fit: BoxFit.contain),
+        //           ),
+        //         );
+        //       }),
+        // ),
+        );
   }
 }
